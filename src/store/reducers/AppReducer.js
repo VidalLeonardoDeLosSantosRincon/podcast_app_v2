b3a7c48aedf8379474121da8d725d1aa6d0eb2da
VidalLeonardoDeLosSantosRincon <niggbibi@hotmail.com>
2022-12-29 22:02:32 -0400

    Configuring darkmode object

--- src/store/reducers/AppReducer.js
+++ src/store/reducers/AppReducer.js
@@ -1,5 +1,21 @@
 const INITIAL_STATE = {
-    dakrmode: false
+    dakrmode : {
+        status : false,
+        theme : {
+            colors: {
+                1 : "white",
+                2 : "lightgray",
+                3 : "dodgerblue",
+                4 : "lightseagreen",
+                5 : "orange"
+            },
+            backgrounds : {
+                1 : "#273746",
+                2 : "#212F3D", 
+                3 : "#1C2833"
+            }
+        }
+    }
 };
 
 export const AppReducer = (state = INITIAL_STATE, action ) => {
